739a089ae098c6425f14a00cc24e7e4c0c15b51c
Ken Sheridan <kensheridan86@gmail.com>
2022-08-08 21:40:00 +0000

    built a view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
-from . import views
+"""
+Docstrings R us!!
+"""
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
